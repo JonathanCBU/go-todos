c36fccc174a3fc1d8df5c0b6d55227c339f4af64
Jonathan Cook <jcookbme@gmail.com>
2025-07-21 10:39:15 -0400

    Csv read write (#2)
    * Set up reading with example csv
    
    * Implement writing single records

--- main.go
+++ main.go
@@ -2,10 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/JonathanCBU/gotodos/cmd"
+	"github.com/JonathanCBU/gotodos/internal/csv"
+	"github.com/JonathanCBU/gotodos/internal/models"
 )
 
 func main() {
-	fmt.Println(cmd.CreateModel())
+	header, body, err := csv.ReadFile("/Users/jonathancook/dev/personal/go-todos/example.csv")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	list, err := models.NewList(
+		"/Users/jonathancook/dev/personal/go-todos/example.csv",
+		header,
+		body,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(list.FileName)
+	fmt.Println(list.Records)
+
+	for idx, row := range list.Records {
+		fmt.Println(idx, row.Description)
+	}
+
+	r1, err := models.NewRecord(
+		list.Headers,
+		[]string{
+			"666",
+			"This is new",
+			"Example description test",
+			"DONE",
+			"2006-01-02 15:04:05",
+			"2006-01-06 15:04:05",
+			"1",
+		},
+	)
+
+	fmt.Println(r1.Writable())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = csv.WriteRecord(*list, *r1)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
